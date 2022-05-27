43fb9cdffa775cf7430c0e676b36b2663503cade
sirgama <sirengogamaliel@gmail.com>
2022-05-27 19:26:32 +0300

    create a save and delete image methods

--- main/models.py
+++ main/models.py
@@ -24,4 +24,8 @@ class Image(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
+    def save_image(self):
+        self.save()
     
+    def delete_image(self):
+        self.delete()
