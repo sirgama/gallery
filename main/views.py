34022732ea142eeea3ad3d4277fdde588379781e
sirgama <sirengogamaliel@gmail.com>
2022-05-27 07:31:53 +0300

    Create home and base templates

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
     
-    return HttpResponse('<h1>Hello Gallery</h1>')
+    return render(request, 'main/home.html')
