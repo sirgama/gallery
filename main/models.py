979f480020ac975c5ec1cd5f704509c68aba71f8
sirgama <sirengogamaliel@gmail.com>
2022-05-27 20:07:19 +0300

    create a classmethod for returning all images

--- main/models.py
+++ main/models.py
@@ -29,3 +29,8 @@ class Image(models.Model):
     
     def delete_image(self):
         self.delete()
+        
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Image, Location, Category
 # Create your views here.
 
 def home(request):
