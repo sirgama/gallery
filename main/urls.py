e37793b505a15d65d53cfa0d7d83b5fb5a7cb1de
sirgama <sirengogamaliel@gmail.com>
2022-05-28 17:58:44 +0300

    create a Clabb based template view in urls and a corresponding view function

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='homepage'),
     path('search/', views.search_results, name='search_results'),
-   
+    path('image/<image_id>/', views.Imager.as_view(), name='imager'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from .models import Image, Location, Category
+from django.views.generic.base import TemplateView
 # Create your views here.
 
 def home(request):
     images = Image.get_images()
     locations = Location.objects.all()
-    return render(request, 'main/home.html', {'images':images, 'locations':locations})
+    categories = Category.objects.all()
+    return render(request, 'main/home.html', {'images':images, 'categories': categories, "locations":locations})
 
 def search_results(request):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
         searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
         
-        return render(request, 'main/search.html', {"message":message, "images":searched_images})
+        return render(request, 'main/search.html', {"message":message, "images":searched_images, 'categories': categories, "locations":locations})
     else:
         message = "You havent searched for any terms"
         return render(request, 'main/search.html', {"message":message})
+    
+
+class Imager(TemplateView):
+    template_name = 'base.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["imagelist"] = Image.objects.all()
+        return context
