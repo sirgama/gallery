27783554a2e4dc18476f0289e0065efb263061de
sirgama <sirengogamaliel@gmail.com>
2022-05-28 08:49:57 +0300

    add a search view function

--- main/views.py
+++ main/views.py
@@ -7,3 +7,13 @@ def home(request):
     locations = Location.objects.all()
     return render(request, 'main/home.html', {'images':images, 'locations':locations})
 
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_description(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'main/search.html', {"message":message, "images":searched_images})
+    else:
+        message = "You havent searched for any terms"
+        return render(request, 'main/search.html', {"message":message})
