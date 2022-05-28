e4924adac9df2493035e7e873028b365e92cdbec
sirgama <sirengogamaliel@gmail.com>
2022-05-29 01:07:06 +0300

    add image detail template for image detailed description

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from . import views
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.home, name='homepage'),
     path('search/', views.search_results, name='search_results'),
     path('image/<image_id>/', views.Imager.as_view(), name='imager'),
-    path(r'details/(\d+)',views.detailed,name ='detailed'),
+    re_path(r'details/(\d+)',views.detailed,name ='detailed'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
