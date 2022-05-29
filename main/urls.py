921c4fc59b499be17df10caf66429b05d909fded
sirgama <sirengogamaliel@gmail.com>
2022-05-29 20:34:21 +0300

    Add a location url path

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('search/', views.search_results, name='search_results'),
     path('image/<image_id>/', views.Imager.as_view(), name='imager'),
     re_path(r'details/(\d+)',views.detailed,name ='detailed'),
+    path('location/<location>/', views.location_filter, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
