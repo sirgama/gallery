df07df08dcbd0f3c8ba308c9734b23947f43e4b1
sirgama <sirengogamaliel@gmail.com>
2022-05-30 18:57:46 +0300

    add copy image link functionality

---
+++ main/static/js/copy.js
@@ -0,0 +1,6 @@
+// console.log('Hello jas')
+
+// var copybtns =document.getElementsByClassName('copy-btn');
+// var text = copybtns.getAttribute('data-hex');
+// document.getElementById('imglink').innerHTML = text;
+// console.log(copybtns)
--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,5 +1,16 @@
+console.log('Hello jas')
 
+const copybtns =[...document.getElementsByClassName('copy-btn')]
+console.log(copybtns)
 
+copybtns.forEach(btn=> btn.addEventListener('click', ()=>{
+    console.log('click')
+    const imageLink = btn.getAttribute('data-hex')
+    console.log(imageLink)
+    navigator.clipboard.writeText(imageLink)
+    btn.textContent = 'Link Copied ^_^'
+    
+}))
 
 // $(".bg3").mouseover(function(){
 //     $(".btn3").show();
